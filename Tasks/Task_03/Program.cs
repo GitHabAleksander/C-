3ef41127d30fecc3eb04f1640477d7ecbc84c688
aleksandr e <aleks@mail.ru>
2023-06-12 17:59:02 +0300

    Add %

---
+++ Tasks/Task_03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число
+//  и выдаёт, является ли число чётным 
+//  (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Add number:");
+string? numberString = Console.ReadLine();
+int numberA = Convert.ToInt32(numberString);
+if(numberA % 2 == 1)
+{
+    Console.Write("Нечетное");
+}
+else
+{
+    Console.Write("Четное");
+}
