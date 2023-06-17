2788c3301a7bd5b20529c65d62f297a69c54d0eb
aleksandr e <aleks@mail.ru>
2023-06-17 18:01:53 +0300

    Исправил графическую ошибку по написанию

---
+++ Tasks/Task_15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введи число:");
+string? number = Console.ReadLine();
+int numDay = Convert.ToInt32(number);
+if (numDay < 8)
+{
+    if(numDay < 5)
+    {
+        Console.Write(" Работаем!");
+    }
+    else
+    {
+        Console.Write(" Выходной!");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня нет");
+}
