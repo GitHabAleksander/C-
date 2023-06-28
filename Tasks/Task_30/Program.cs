342f25887dc8bffbc8e8841506e53ac280322d90
aleksandr e <aleks@mail.ru>
2023-06-28 21:31:56 +0300

    First commit without answer

---
+++ Tasks/Task_30/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+// int ReadInt()
+// {
+//     Console.WriteLine("Введите число N:");
+//     int value = Convert.ToInt32(Console.ReadLine());
+//     return value;
+// }
+
+// int Counter(int N)
+// {
+//     int NN = 1;
+//     for(int i = 1; i <= N; i++)
+//     {
+//         NN*= i;
+//     }
+//     return NN;
+// }
+// int number = ReadInt();
+// int coun = Counter(number);
+// Console.WriteLine(coun);
