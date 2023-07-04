79f71d686e38accce8f5b3705ad5b0adcff40483
aleksandr e <aleks@mail.ru>
2023-07-04 21:24:10 +0300

    First commit

--- Tasks/Task_25/Program.cs
+++ Tasks/Task_25/Program.cs
@@ -3,3 +3,34 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+int ReadInt(string massage)
+{
+    Console.Write(massage);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int Counter(int A, int exp)
+{
+    int B = 1;
+    for (int i = 0; i < exp; i++)
+    {
+        B *= A;
+    }
+    return B;
+}
+bool ValidateExponent(int exp)
+{
+    if(exp < 0)
+    {
+        Console.WriteLine("Показатель не меньше нуля");
+        return false;
+    }
+    return true;
+}
+int A = ReadInt("Введите основание числа: ");
+int exp = ReadInt("Введите показатель: ");
+if(ValidateExponent(exp))
+{
+    Console.WriteLine($"Число {A} в степени {exp} равен {Counter(A, exp)}");
+}
