04bfd74b7c85aab03c90959f0271549d174b3479
aleksandr e <aleks@mail.ru>
2023-07-04 20:50:02 +0300

    First commit

--- Tasks/Task_27/Program.cs
+++ Tasks/Task_27/Program.cs
@@ -3,3 +3,23 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int ReadInt()
+{
+    Console.WriteLine("Введите число:");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int SumCounter(int a)
+{
+    int result = 0;
+        while(a > 0)
+    {
+       result += a % 10;
+       a = a / 10;
+    }
+    return result;
+}
+int a = ReadInt();
+int coun = SumCounter(a);
+Console.WriteLine(coun);
