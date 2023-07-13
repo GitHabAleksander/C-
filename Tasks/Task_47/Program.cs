01dbcd96c5323bbb5e68d4ea4497c6ae176986a2
aleksandr e <aleks@mail.ru>
2023-07-13 22:18:38 +0300

    Добавил double

--- Tasks/Task_47/Program.cs
+++ Tasks/Task_47/Program.cs
@@ -9,28 +9,28 @@
 const int ROWS = 3;
 const int COLUMS = 4;
 
-int[,] RandomMatrix(int rows, int colums)
+double[,] RandomMatrix(int rows, int colums)
 {
-    int[,] matrix = new int[rows, colums];
+    double[,] matrix = new double[rows, colums];
     for(int i = 0; i < matrix.GetLength(0); i++)
     {
-        for( int j = 0; j < matrix.GetLength(1); j++)
+        for(int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = Random.Shared.Next(-10, 10);
+            matrix[i, j] = Math.Round(Random.Shared.Next(-100, 101) * 0.1, 1);
         }
     }
     return matrix;
 }
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(double[,] matrix)
 {
     for(int i = 0; i < matrix.GetLength(0); i++)
     {
-        for( int j = 0; j < matrix.GetLength(1); j++)
+        for(int j = 0; j < matrix.GetLength(1); j++)
         {
             Console.Write($"{matrix[i, j]}\t");
         }
         Console.WriteLine();
     }
 }
-int[,] myMatrix = RandomMatrix(ROWS, COLUMS);
+double[,] myMatrix = RandomMatrix(ROWS, COLUMS);
 PrintMatrix(myMatrix);
