b31ed1fd96c8f9ac9e11267124d0f832ba868c36
aleksandr e <aleks@mail.ru>
2023-07-16 18:30:26 +0300

    Решение без вариантов N

---
+++ Tasks/Task_64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number = 10;
+
+void NaturChisla(int LastNumber)
+{
+    if(LastNumber <= 0) return;
+    NaturChisla(LastNumber - 1);
+    Console.Write((number - LastNumber + 1) + " ");
+}
+NaturChisla(number);
