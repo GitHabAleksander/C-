389eee5a53898a209f20c1f589b1d5252b5f3cfc
aleksandr e <aleks@mail.ru>
2023-07-16 18:36:26 +0300

    Проработал вариацию введения числа

--- Tasks/Task_64/Program.cs
+++ Tasks/Task_64/Program.cs
@@ -3,7 +3,8 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-int number = 10;
+Console.WriteLine("Введите N:");
+int number = Convert.ToInt32(Console.ReadLine());
 
 void NaturChisla(int LastNumber)
 {
